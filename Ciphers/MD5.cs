d65189b471a22f058e9631e2c2dc5cabba1920ed
Asandei Stefan <52822996+NikolaTesla13@users.noreply.github.com>
2020-05-25 18:04:31 +0300

    Create MD5.cs

---
+++ Ciphers/MD5.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MD5Sample
+{
+    class MD5Sample
+    {
+        static string Encrypt(string input)
+        {
+            MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+    }
+}
